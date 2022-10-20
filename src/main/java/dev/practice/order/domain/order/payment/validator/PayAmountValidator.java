cc57e3ecc5441109b3386fdc816ce659b558f651
H199001181561 <jsj1001@hyundai-autoever.com>
2022-10-20 14:35:59 +0900

    feat($pay): add logs to validate price

--- src/main/java/dev/practice/order/domain/order/payment/validator/PayAmountValidator.java
+++ src/main/java/dev/practice/order/domain/order/payment/validator/PayAmountValidator.java
@@ -4,14 +4,18 @@ package dev.practice.order.domain.order.payment.validator;
 import dev.practice.order.common.exception.InvalidParamException;
 import dev.practice.order.domain.order.Order;
 import dev.practice.order.domain.order.OrderCommand;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @org.springframework.core.annotation.Order(value = 1)
 @Component
+@Slf4j
 public class PayAmountValidator implements PaymentValidator{
     @Override
     public void validate(Order order, OrderCommand.PaymentRequest request) {
         if (!order.getTotalPrice().equals(request.getAmount())) {
+            log.info("Req Price = {}", request.getAmount());
+            log.info("Orginal Price = {}", order.getTotalPrice());
             throw new InvalidParamException("주문가격이 불일치합니다");
         }
     }
